872e1e41ca1896628bb3410057ac0fc3b2cc7da9
Shadkoech <shadrackipruto@gmail.com>
2024-04-11 12:41:17 +0300

    Method to encrypt user passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -17,3 +17,17 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode(), salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks is a password matches the hashed password
+    Args:
+        hashed_password (bytes): The hashed password
+        password (str): The password to check
+    Returns:
+        bool: True if password matches hashed password, False otherwise"""
+    # use bcrypt.checkpw() to validate whether provided \
+    # plain text password matches the hashed password.
+    # encode() method is used to convert a string to its corresponding bytes
+
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
