7e0fe9409e412f0299e56a17adf449d0ebf8257f
Shadkoech <shadrackipruto@gmail.com>
2024-04-16 08:28:55 +0300

    Basic auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that implements Basic authentication"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic authentication management class"""
+    pass
