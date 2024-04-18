07f26df9f93959babc3e04e6e0393c0affb9b38d
Shadkoech <shadrackipruto@gmail.com>
2024-04-18 11:06:31 +0300

    Creating an empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that implements session authentication"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Class for session-based authentication"""
+    pass
