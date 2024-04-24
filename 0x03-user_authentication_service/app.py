1f2ede266f89440ea33e9cccdf46d7a2832f9390
Shadkoech <shadrackipruto@gmail.com>
2024-04-24 23:00:05 +0300

    Implement Login function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Module that sets up a flask app"""
 
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 
 AUTH = Auth()
 app = Flask(__name__)
@@ -25,6 +25,28 @@ def users():
         return jsonify({"email": email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Endpoint to login user based on provided email & password
+    Returns:
+        JSON response with session ID, otherwise abort"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password:
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    if session_id:
+        msg = {"email": email, "message": "logged in"}
+        response = jsonify(msg)
+        # Store session ID as a cookie
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
