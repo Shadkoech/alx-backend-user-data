37b0413e2302b77ea5dae9e3a3eec61b5f64b109
Shadkoech <shadrackipruto@gmail.com>
2024-04-25 11:28:50 +0300

    A route to reset the token password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -120,8 +120,10 @@ class Auth:
 
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = self._generate_uuid()
-            self._db.update_user(user.id, reset_token=reset_token)
-            return reset_token
-        except ValueError:
-            return None
+        except NoResultFound:
+            raise ValueError
+
+        reset_token = self._generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+
+        return reset_token
